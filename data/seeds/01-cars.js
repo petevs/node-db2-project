56ea91858880de3ebf65caf1dd1204a2ef4ab31f
Pete VS <pete_vs88@me.com>
2021-02-28 11:45:45 -0500

    migration working, seed working

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,16 @@
 // STRETCH
+exports.seed = async function(knex) {
+    await knex("cars").truncate()
+    await knex("cars").insert([
+        {
+            vin: "v80234",
+            make: "ford",
+            model: "f350",
+            mileage: 250000,
+            title: "anonymous",
+            transmission: "something"
+        }
+    ])
+
+
+}
