6b32b5f62c699388760c365307901a9f41775a76
Pete VS <pete_vs88@me.com>
2021-02-28 12:04:51 -0500

    getall, get by id, create linked up

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,35 @@
 // DO YOUR MAGIC
+const router = require('express').Router()
+const { whereNotExists } = require('../../data/db-config')
+const cars = require('./cars-model')
+
+//Get Cars
+router.get('/', async (req, res) => {
+    try {
+        const results = await cars.getAll()
+        res.json(results)
+    } catch(err) {
+        res.status(400).json(err)
+    }
+})
+
+//Get Car By ID
+router.get('/:id', async (req, res) => {
+    try {
+        const result = await cars.getById(req.params.id)
+        res.json(result)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.post('/', async ( req, res, next) => {
+    try{
+        const result = await cars.create(req.body)
+        res.json(result)
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
